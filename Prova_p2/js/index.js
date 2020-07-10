857646e00578bd923b123656120d1f9b569ec371
jjucaa <joaovitorsilveriocoelho@yahoo.com.br>
2020-07-09 22:11:11 -0300

    Add files via upload

---
+++ Prova_p2/js/index.js
@@ -0,0 +1,20 @@
+
+function excel(){
+    
+}
+
+function sub_cel(cel1, cel2){
+    let x = cel1;
+    let y = cel2;
+    let result = x - y;
+
+    return(result);
+}
+
+function soma_cel(cel1, cel2){
+    let x = cel1;
+    let y = cel2;
+    let result = x + y;
+
+    return(result);
+}
